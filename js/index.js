37f7b114289fc72e2a127d8d31f4f64d2bb6865b
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2021-12-30 20:41:05 +0530

    used supah code for animation
    https://codepen.io/supah/pen/dGLLPK

--- js/index.js
+++ js/index.js
@@ -1,46 +1,54 @@
-configData = null
+var configData = null
+var lFollowX = 5
+var lFollowY = 10
+var x = 0
+var y = 0
+var friction = 1 / 20
+
 $(()=>{
-    getConfigData((data) => {
+    animate()
+    getConfigData((data)=>{
         configData = data
-        addSocials()
         addCardImage()
-        cardMove()
+        addSocials()
         addText()
     })
 })
-
-function addSocials(){
-    for (const key in configData.links) {
-        $('.socials').append(`<a href="${configData.links[key][0]}"><img class="icon" src="${configData.links[key][1]}"></a>`);
-    }
-}
-
-function addCardImage(){
-    $(".card").css("background-image",`url("${configData.card_image}")`)
-}
-
-function cardMove(){
-    $("body").mousemove((event) => {
-        var objLeft = $("body").offset().left;
-        var objTop = $("body").offset().top;
-
-        var objCenterX = objLeft + $("body").width() / 2;
-        var objCenterY = objTop + $("body").height() / 2;
-        var x = event.pageX - objCenterX
-        var y = event.pageY - objCenterY
-
-        $(".card").css("transform",`perspective(500px) rotateX(${-y/70}deg) rotateY(${x/70}deg)`)
-    })
-}
-
-function addText(){
-    $(".text").html(configData.text)
-}
 
 function getConfigData(callback){
     $.getJSON("../utils/config.json", (data) => {
         callback(data)
     }).fail(() => {
-        console.log("An error has occurred");
-    });
+        console.log("An error has occurred")
+    })
+}
+
+function animate() {
+  x += (lFollowX - x) * friction
+  y += (lFollowY - y) * friction
+  $('.wrapper').css({
+    'transform': 'translate(-50%, -50%) perspective(600px) rotateY(' + -x + 'deg) rotateX(' + y + 'deg)'
+  })
+  window.requestAnimationFrame(animate)
+}
+
+$(window).on('mousemove click', function(e) {
+  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX))
+  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY))
+  lFollowX = (12 * lMouseX) / 100 // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (10 * lMouseY) / 100
+})
+
+function addText(){
+    $(".text").html(configData.text)
+}
+
+function addCardImage(){
+    $(".me").css("background-image",`url("${configData .card_image}")`)
+}
+
+function addSocials(){
+    for (const key in configData.links) {
+        $('.socials').append(`<a href="${configData.links[key][0]}"><img class="icon" src="${configData.links[key][1]}"></a`)
+    }
 }
