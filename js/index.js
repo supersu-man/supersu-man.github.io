699eaa7ae0ad09ccc380b9a0beb6e374502f5695
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2023-01-08 13:41:44 +0530

    portfolio v3

--- js/index.js
+++ js/index.js
@@ -1,20 +1,35 @@
-var toggle = document.getElementById('themeSwitch')
-var body = document.getElementById('body')
-
-if (localStorage.getItem('dark-theme')) {
-    body.classList.remove('light-theme')
-    body.classList.add('dark-theme')
-    toggle.setAttribute('checked', 'true')
-} 
-
-toggle.addEventListener('change', () => {
-    if (toggle.checked) {
-        body.classList.remove('light-theme')
-        body.classList.add('dark-theme')
-        localStorage.setItem('dark-theme', true)
-    } else {
-        body.classList.add('light-theme')
-        body.classList.remove('dark-theme')
-        localStorage.removeItem('dark-theme')
-    }
+$(window).on('load', () => {
+    $('.marquee').marquee({
+        duplicated: true,
+        duration: 10000
+    });
+    $('.marquee-faster').marquee({
+        duplicated: true,
+        duration: 6000
+    });
 })
+
+$('#highlight').click(() => {
+    window.open("https://play.google.com/store/apps/details?id=com.supersuman.gitamite");
+})
+
+$('#about').click(() => {
+    window.open("https://www.linkedin.com/in/sumanthperambuduri/");
+})
+
+$('#projects').click(() => {
+    window.open("https://github.com/supersu-man");
+})
+
+$('#contact').click(() => {
+    window.open("https://mail.google.com/mail/?view=cm&source=mailto&to=sumanthpera444@gmail.com")
+})
+
+$('.real-card').hover(
+    () => {
+        $('#highlight').addClass('highlight-hover')
+    },
+    () => {
+        $('#highlight').removeClass('highlight-hover')
+    }
+)
