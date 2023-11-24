d45e60481ac2628f3198fb6249b3aba3757ee8fb
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2023-11-24 12:11:38 +0530

    added cycling card and pictures

---
+++ cycling.js
@@ -0,0 +1,39 @@
+let eventTemplate = (heading, date, images) => {
+
+    let imageList = `<div class="col-12 p-1 fs-3">${heading}</div>`
+    images.forEach(image => {
+        imageList+=`
+            <div class="col-auto p-1">
+                <img class="img-fluid" src="assets/images/${image}" />
+            </div>
+        `
+    });
+
+    return `
+        <div class="row pt-4">
+            ` + 
+            imageList
+            +
+            `
+        </div>
+    `
+}
+
+this.loadJSON((response) => {
+    let data = JSON.parse(response)
+    let eventList = ''
+    data.forEach(eventt => {
+        eventList+=eventTemplate(eventt.heading, eventt.date, eventt.images)
+    })
+    this.document.body.innerHTML = eventList
+})
+
+function loadJSON(callback) {
+    var xObj = new XMLHttpRequest();
+    xObj.overrideMimeType("application/json");
+    xObj.open('GET', './cycling.json', true);
+    xObj.onreadystatechange = () => {
+        if (xObj.readyState === 4 && xObj.status === 200) callback(xObj.responseText); 
+    };
+    xObj.send(null);
+}
